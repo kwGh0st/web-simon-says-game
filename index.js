94eef0f79b799d04140fd035a728bfae0ff18cdf
Karol Programuje <karol341654@gmail.com>
2023-06-06 23:30:25 +0200

    Adding sounds, creating playAudio function

--- index.js
+++ index.js
@@ -3,10 +3,39 @@ var colors = ['red', 'blue', 'green', 'yellow'];
 var gamePattern = [];
 
 function nextSequence() {
+    
     var random = Math.floor(Math.random() * 3);
     var randomChosenColour = colors[random];
     gamePattern.push(randomChosenColour);
+    $('.' + gamePattern[0]).addClass('pressed');
+    playAudio(gamePattern[0]);
+    setTimeout( function () {
+        $('.' + gamePattern[0]).removeClass('pressed')
+    } , 300);
+   
 }
 
+function playAudio(color) {
+    switch(color) {
+        case 'blue':
+            var audio = new Audio('./sounds/blue.mp3');
+            audio.play();
+            break;
 
+        case 'red':
+            var audio = new Audio('./sounds/red.mp3');
+            audio.play();
+            break;
 
+        case 'yellow':
+
+            var audio = new Audio('./sounds/yellow.mp3');
+            audio.play();
+            break;
+
+        case 'green':
+            var audio = new Audio('./sounds/green.mp3');
+            audio.play();
+            break;
+    }
+}
